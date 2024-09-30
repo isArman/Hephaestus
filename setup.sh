5b998302adf1937351803ab13e7b102d4aae4aa4
Arman Abdi <armanabdi@email.com>
2024-09-30 18:18:37 +0330

    add connectivity file to bypass access from Iran

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ sudo sed -i 's/enabled=1/enabled=0/g' "/etc/yum.repos.d/"/*
 sudo cp ./iutfedora.repo /etc/yum.repos.d/.
 sudo dnf update -y
 
-# 
+# Modify the default network status endpoint to bypass access issues from Iran.
+sudo cp ./90-connectivity.conf /etc/NetworkManager/conf.d/.
