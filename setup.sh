87168937624358d1d250a016925121a0956608df
Arman Abdi <armanabdi@email.com>
2024-09-30 18:43:53 +0330

    add transmission installation

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ sudo dnf update -y
 sudo cp ./90-connectivity.conf /etc/NetworkManager/conf.d/.
 
 # Install some packages there are in dnf repository
-sudo dnf install vim zsh docker
+sudo dnf install vim zsh docker transmission
 
 # enable docker
 sudo systemctl enable --now docker.service
@@ -46,5 +46,3 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf check-update
 sudo dnf install code # or code-insiders
-
-
