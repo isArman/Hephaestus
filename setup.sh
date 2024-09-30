0ca63236eadca96eb2aa35d95340665333e1eb17
Arman Abdi <armanabdi@email.com>
2024-09-30 19:16:40 +0330

    add warning and messages

--- setup.sh
+++ setup.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+
 # Change the default repository to iutfedora repository to bypass restrictions.
+sudo rm /etc/yum.repos.d/iutfedora.repo
 sudo sed -i 's/enabled=1/enabled=0/g' "/etc/yum.repos.d/"/*
 sudo cp ./iutfedora.repo /etc/yum.repos.d/.
 sudo dnf upgrade -y
 
+echo -e "${GREEN}Default repository changed successfully.${NC}"
+
 # Modify the default network status endpoint to bypass access issues from Iran.
 sudo cp ./90-connectivity.conf /etc/NetworkManager/conf.d/.
+echo -e "${GREEN}Default network status endpoint changed successfully.${NC}"
 
 # Install some packages there are in dnf repository
 sudo dnf install vim zsh docker transmission -y
+echo -e "${GREEN}Packages installed successfully.${NC}"
 
 # enable docker
 sudo systemctl enable --now docker.service
+echo -e "${GREEN}Docker service enabled successfully.${NC}"
 
 # Install v2rayA using docker (It's accesible in localhost:2017)
 sudo docker pull docker.arvancloud.ir/mzz2017/v2raya
@@ -30,17 +38,21 @@ sudo docker run -d \
   -v /etc/resolv.conf:/etc/resolv.conf \
   -v /etc/v2raya:/etc/v2raya \
   mzz2017/v2raya
+echo -e "${GREEN}v2rayA is accessible at http://localhost:2017 .${NC}"
 
 # Setup oh-my-zsh! (a delightful, open source, community-driven framework for managing your Zsh configuration)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo -e "${GREEN}oh-my-zsh set up successfully.${NC}"
 
 # Install Telegram
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install telegram
+echo -e "${GREEN}Telegram installation completed successfully.${NC}"
 
 # Install vscode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-dnf check-update
+sudo dnf check-update
 sudo dnf install code # or code-insiders
+echo -e "${GREEN}vscode installation completed successfully.${NC}"
