e91cbdbe7ab2f8d3436453f43bcb1b040a31dea6
Arman Abdi <armanabdi@email.com>
2024-09-30 15:38:06 +0330

    change default repository

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
-echo "Hello World"
+#!/bin/bash
+
+# Change the default repository to iutfedora repository to bypass restrictions.
+sed -i 's/enabled=1/enabled=0/g' "/etc/yum.repos.d/"/*
+sudo cp ./iutfedora.repo /etc/yum.repos.d/.
+sudo update -y
